ddf2fd42b7911c792fd820f44b60634fe11bc1c3
Fadi Qassem <fqassem@ncsu.edu>
2019-07-01 17:44:49 -0700

    adding create post and get blog post functionality

---
+++ diary-backend/routes/posts.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+/* Our Mock little database with Model */
+let posts = [];
+function BlogPost(title, content) {
+  this.title = title;
+  this.content = content;
+  this.datePublished = new Date();
+};
+
+const bp1 = new BlogPost('Hello', 'Lots of stuff');
+posts.push(bp1);
+
+/* GET all Blog Posts */
+router.get("/all", function(req, res, next) {
+  res.status(200).json({ posts: posts });
+});
+
+/* POST to create a blog post */
+router.post("/createPost", function(req, res, next) {
+  const { body } = req;
+  const { title, content } = body;
+  let errors = [];
+
+  if (!body.title || body.title.length == 0) {
+    errors.push("Title is missing");
+  }
+
+  if (!body.content || body.content.length == 0) {
+    errors.push("Content is missing");
+  }
+
+  if (errors.length) {
+    res.status(422).send(errors);
+  } else {
+    posts.unshift(new BlogPost(title, content)); //adds to front of array so it's sorted by default
+    res.status(200).send("Successfully added post");
+  }
+});
+
+module.exports = router;
