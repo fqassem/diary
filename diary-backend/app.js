b433daf246f5ebaff46679d527cfb604d3419e6d
Fadi Qassem <fqassem@ncsu.edu>
2019-07-02 15:32:02 -0700

    adding firebase to backend

--- diary-backend/app.js
+++ diary-backend/app.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv').config();
+const fbAdmin = require('./firebase');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +13,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const verifyToken = async (req, res, next) => {
+    const {authorization}= req.headers;
+    try {
+        const decodedIdToken = await fbAdmin.auth().verifyIdToken(authorization);
+        if(decodedIdToken) {
+            req.body.uid = decodedIdToken.uid;
+            return next();
+        } else {
+            return res.status(401).send("You are unauthorized");
+        }
+    } catch(error) {
+        res.status(401).send("You are unauthorized");
+    }
+};
+
+app.use('/', verifyToken);
 app.use('/posts', postsRouter);
 
 module.exports = app;
